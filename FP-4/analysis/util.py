a2139a25847b4fe999105c1982de0479a5904cc1
Beanrico <beanrico@icloud.com>
2025-12-03 18:24:13 +0900

    delete

--- FP-4/analysis/util.py
+++
@@ -1,9 +0,0 @@
-from IPython.display import display,Markdown
-def display_title(s, pref='Figure', num=1, center=False):
-    ctag = 'center' if center else 'p'
-    s    = f'<{ctag}><span style="font-size: 1.2em;"><b>{pref} {num}</b>: {s}</span></{ctag}>'
-    if pref=='Figure':
-        s = f'{s}<br><br>'
-    else:
-        s = f'<br><br>{s}'
-    display( Markdown(s) )
